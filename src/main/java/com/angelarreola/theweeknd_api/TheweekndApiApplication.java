fc91c3017698b0410d9f9358f5fa7b5b286038b1
Angel Arreola <angelarreolagg@gmail.com>
2025-02-20 02:12:57 -0600

    🎉 init: First Spring Boot project setup

---
+++ src/main/java/com/angelarreola/theweeknd_api/TheweekndApiApplication.java
@@ -0,0 +1,13 @@
+package com.angelarreola.theweeknd_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheweekndApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheweekndApiApplication.class, args);
+	}
+
+}
